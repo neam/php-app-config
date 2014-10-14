9f405cbf3702ba1f5531ffb225803d635cc613be
Fredrik Wollsén <fredrik@neam.se>
2014-10-14 13:51:36 +0200

    Corrected require paths

--- common.php
+++ common.php
@@ -2,17 +2,19 @@
 
 namespace neam\bootstrap;
 
+$extension_root = dirname(__FILE__);
+$project_root = $extension_root . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..';
+
 // Make the helper classes available without requiring autoloading
 if (!class_exists('Config', false)) {
-    require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'helpers' . DIRECTORY_SEPARATOR . 'Config.php');
+    require($extension_root . DIRECTORY_SEPARATOR . 'helpers' . DIRECTORY_SEPARATOR . 'Config.php');
 }
-if (!class_exists('Dotenv', false)) {
-    require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'vendor/vlucas/phpdotenv/src/Dotenv.php');
+if (!class_exists('\Dotenv', false)) {
+    require($project_root . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'vlucas' . DIRECTORY_SEPARATOR . 'phpdotenv' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Dotenv.php');
 }
 
 // Define what application-specific config to expect - see README.md for more information
 $config_include = Config::read('CONFIG_INCLUDE', $default = 'config/local/include.php', $required = false);
-$project_root = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..';
 $config = $project_root . DIRECTORY_SEPARATOR . $config_include;
 if (!is_readable($config)) {
     echo "PHP app config file not available ($config).";
