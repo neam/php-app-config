b94a83e0328ce050547e5100261a07713fdec813
Fredrik Wollsén <fredrik@neam.se>
2016-05-25 11:18:57 +0300

    Not conflicting with unix-tmpdir env var

--- shell-export.sh
+++ shell-export.sh
@@ -2,18 +2,18 @@
 # Should be sourced from the directory containing the vendor folder
 # Note: No shebang since this file is supposed to be sourced
 
-export TMPDIR="$(mktemp -d)"
+PHP_APP_CONFIG_TMPDIR="$(mktemp -d)"
 export PHP_APP_CONFIG_EXPORTED=0
-php -d variables_order="EGPCS" vendor/neam/php-app-config/export.php > $TMPDIR/php-app-config.sh
+php -d variables_order="EGPCS" vendor/neam/php-app-config/export.php > $PHP_APP_CONFIG_TMPDIR/php-app-config.sh
 
 if [ "$?" == "0" ]; then
-    source $TMPDIR/php-app-config.sh
+    source $PHP_APP_CONFIG_TMPDIR/php-app-config.sh
     export PHP_APP_CONFIG_EXPORTED=1
 else
     # show error messages
-    cat $TMPDIR/php-app-config.sh
+    cat $PHP_APP_CONFIG_TMPDIR/php-app-config.sh
 fi
 
-rm -r "$TMPDIR"
+rm -r "$PHP_APP_CONFIG_TMPDIR"
 
 [ "$PHP_APP_CONFIG_EXPORTED" == "1" ] || echo "Warning: The PHP app config was not exported successfully";
